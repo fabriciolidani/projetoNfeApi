af7fe65dcf8cce7099d3cfae1ffd5d5fa3aee48d
fabriciolidani <fabriciolidani96@gmail.com>
2023-04-10 14:16:32 -0300

    mais um

--- routes/sefaz/consultaCNPJ.js
+++ routes/sefaz/consultaCNPJ.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
 const { DistribuicaoDFe, RecepcaoEvento } = require('node-mde')
 const unzipper = require('unzipper');
-const consultaCnpjTeste = require ("consultar-cnpj")
+const consultaCnpjTeste = require("consultar-cnpj")
+const InformacoesManifesto = require('../../models/InformacoesManifesto')
+const adicionarNsu = require('../database/adicionarNsu');
 
 module.exports = async (req, res) => {
   const cnpj = req.params.cnpj;
   const data = req.body.cnpj;
-
+  const criarNsu = {
+    idNsu:"TESTE",
+    cnpj:"TESTE",
+    nome:"TESTE",
+    nfe:"TESTE",
+    valor:"TESTE",
+    tipo:"TESTE",
+    situacao:"TESTE",
+    numero:"TESTE",
+    ie:"TESTE",
+    emissao:data,
+}
   try {
-  const testeCNPJ = await consultaCnpjTeste(data)
+    const testeCNPJ = await consultaCnpjTeste(data)
+
+    const testeAdicaoo = adicionarNsu(criarNsu)
     res.send(testeCNPJ);
   } catch (error) {
     console.error(error);
